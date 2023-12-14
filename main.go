49d7e0b99d80a5edcaabeefba8c23736cd8fc3c4
Light <lochois@gmail.com>
2023-12-14 21:50:41 +1100

    add components

--- main.go
+++ main.go
@@ -2,21 +2,33 @@ package main
 
 import (
 	"embed"
-	_ "embed"
 	"log"
+	"net/http"
 
+	"wails-template/components"
+
+	"github.com/a-h/templ"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/wailsapp/wails/v3/pkg/application"
 )
 
 //go:embed frontend/dist
 var assets embed.FS
+var version = "0.0.0"
 
 func main() {
+	r := NewChiRouter()
+
 	app := application.New(application.Options{
 		Name:        "wails3-htmx-template",
 		Description: "A demo of using raw HTML & CSS",
 		Assets: application.AssetOptions{
 			FS: assets,
+			Middleware: func(next http.Handler) http.Handler {
+				r.NotFound(next.ServeHTTP)
+				return r
+			},
 		},
 		Mac: application.MacOptions{
 			ApplicationShouldTerminateAfterLastWindowClosed: true,
@@ -42,11 +54,34 @@ func main() {
 	}
 }
 
+func NewChiRouter() *chi.Mux {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/initial", templ.Handler(components.Pages([]struct {
+		Path  string
+		Label string
+	}{
+		{"/greet", "Greet form"},
+	}, struct {
+		Version string
+		Text    string
+	}{
+		version, "No update available",
+	})).ServeHTTP)
+	r.Get("/greet", templ.Handler(components.GreetForm("/greet")).ServeHTTP)
+	r.Post("/greet", components.Greet)
+	r.Get("/modal", templ.Handler(components.TestPage("#modal", "outerHTML")).ServeHTTP)
+	r.Post("/modal", templ.Handler(components.ModalPreview("Title for the modal", "Sample Data")).ServeHTTP)
+	return r
+}
+
 // TODO
 // - Add support for htmx
 // no js
 //chi
-//tailwind
+//tailwind - done
 //daisyui
 //systray
 //multiwindow
