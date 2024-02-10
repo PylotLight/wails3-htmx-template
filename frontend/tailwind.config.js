55f7494e92f64814e959560c123382a6d2d29541
Lochy <lochois@gmail.com>
2024-02-10 13:34:02 +1100

    fix file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-<<<<<<< HEAD
 module.exports = {
-=======
-export default {
->>>>>>> a0c6c835ed1f58f75ec5190283726ba800e37a84
     content: ["../components/*.{go,templ}", "./index.html", './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',],
     theme: {
         extend: {},
